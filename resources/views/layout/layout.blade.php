7b9b549028b99c93cbb19ebeefcfbbc8ac7a4f45
Seno <senoa5995@gmail.com>
2025-12-25 19:43:47 +0700

    test

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -19,7 +19,7 @@
 
   <!-- kotak kreator -->
 
-  <nav style="background-image: url(img/bg.jpg);" class="bg-white border-gray-200 dark:bg-gray-900 ">
+  <nav style="background-image: url(img/bg.jpg);" class="bg-white border-gray-200 dark:bg-gray-900">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 
       <!-- logo  -->
@@ -33,7 +33,7 @@
         </span>
       </div>
       <!-- search  -->
-      <div class="flex md:order-2 items-center gap-2">
+      <div class="flex md:order-2 items-center gap-2 ">
         <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
           <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
